c3a13d12f7c34b5dad166128d250b1f8d0d3d75a
Martin <mkmartinoes@gmail.com>
2022-05-08 16:16:33 +0300

    display information on card

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,33 @@
 import React from "react";
 // import ListingCard from "./ListingCard";
+import { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
+
+
 
 function ListingsContainer() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(res => res.json())
+    .then( res => {
+      setItems(res)
+    })
+  }, [])
+
+  const itemToPost = items.map(item => (
+    <ListingCard id = {item.id}
+     description = {item.description} 
+     image = {item.image} 
+     location = {item.location} 
+    />
+  ))
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {itemToPost}
       </ul>
     </main>
   );
